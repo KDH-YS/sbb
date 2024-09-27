84316ad7fdfd53305c25bdf3d5d0efbc99ea2d05
KDH <gloian@naver.com>
2024-09-27 14:29:20 +0900

    m

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,16 +1,27 @@
 package com.example.demo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody; 
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller 
 public class HelloController { 
 
-	
+    @GetMapping("/") 
+    @ResponseBody 
+    public String saying() { 
+        return "<form action='/hello' method='get'>"
+                + "<button type='submit'>명언 보기</button>"
+                + "</form>"+"<form action='/e' method='get'>"
+                + "<button type='submit'>이벤트</button>"
+                + "</form>"; 
+    } 
 	@GetMapping("/hello")
-	@ResponseBody
-	public String hello() {
-		return "Hello World";
+//	@ResponseBody
+	public String hello(Model model) {
+		model.addAttribute("message", "Hello, Thymeleaf");
+		return "hello";
 	}
 
 	
