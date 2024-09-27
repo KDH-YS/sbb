861c8937732f2191cd24e51461b57ace396d5648
KDH <gloian@naver.com>
2024-09-27 10:24:37 +0900

    123

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,30 +1,9 @@
 package com.example.demo;
 import org.springframework.stereotype.Controller; 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody; 
 
 @Controller 
 public class HelloController { 
-    @GetMapping("/") 
-    @ResponseBody 
-    public String saying() { 
-        return "<form action='/hello' method='post'>"
-                + "<button type='submit'>명언 보기</button>"
-                + "</form>"+"<form action='/e' method='get'>"
-                + "<button type='submit'>이벤트</button>"
-                + "</form>"; 
-    } 
-    @PostMapping("/hello") 
-//    @ResponseBody 
-    public String hello() { 
-        return "hello.html";
-        		 
-    } 
-    
-    @GetMapping("/e")
-    public String event() {
-    	String filename="event";
-    	return filename;
-    }
+
+
+	
 }
